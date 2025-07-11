c445cd987a0198e6208b6b475fb3a0d600d9d595
darkot1 <sebastian.serna.1998@gmail.com>
2025-07-10 19:29:47 -0500

    feat(auth): implement login with JWT and 401 error handling on authentication

--- src/main/java/com/restora/user/infrastructure/adapter/SpringAuthenticationAdapter.java
+++ src/main/java/com/restora/user/infrastructure/adapter/SpringAuthenticationAdapter.java
@@ -4,6 +4,7 @@ import com.restora.user.application.port.out.AuthenticationPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,10 +12,17 @@ import org.springframework.stereotype.Component;
 public class SpringAuthenticationAdapter implements AuthenticationPort {
 
     private final AuthenticationManager authenticationManager;
+
     @Override
     public void authenticate(String email, String password) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(email, password)
-        );
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(email, password)
+            );
+        } catch (AuthenticationException ex) { // Captura la de Spring
+            throw new com.restora.config.exception.AuthenticationException(
+                    "Credenciales inválidas, por favor verifica e intenta nuevamente."
+            );
+        }
     }
 }
